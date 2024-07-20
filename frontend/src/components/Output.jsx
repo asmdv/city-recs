54de98f8303c0e21301d5b8bda7c9136ca309643
Aaron Alexander <t-aalexander@microsoft.com>
2024-07-20 14:27:06 -0700

    add carousel

--- frontend/src/components/Output.jsx
+++ frontend/src/components/Output.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from 'react';
+import ImageCarousel from './ImageCarousel';
 
 export default function Output(props) {
+    const images = [
+        {
+          url: 'https://wallpaperaccess.com/full/123595.jpg',
+          alt: 'Seattle',
+          caption: 'Seattle'
+        },
+        {
+          url: 'https://th.bing.com/th/id/OIP.Gckhk5qK1uzzwoS34rfCFwAAAA?rs=1&pid=ImgDetMain',
+          alt: 'Space Needle',
+          caption: 'Space Needle'
+        },
+        {
+          url: 'https://cdn1.vox-cdn.com/uploads/chorus_image/image/47552879/Pike_Place_Market_Entrance.0.0.jpg',
+          alt: 'Pike Place Market',
+          caption: 'Pike Place Market'
+        }
+      ];
+
   return (
     <div>
     <img src={props.image} style={{ width: '100%', maxWidth: '500px', margin: 'auto', marginTop: '10px', padding: '5px', borderWidth: '1px', borderRadius: 'lg' }} />
     Congrats, you will love: 
     <h2 style={{ fontSize: '2rem', fontWeight: 'bold' }}>{props.city}</h2>
     {props.text}
-    <img src={props.landmark} alt={props.city} style={{ width: '100%', maxWidth: '500px', margin: 'auto', marginTop: '10px', padding: '5px', borderWidth: '1px', borderRadius: 'lg' }} />
+    <ImageCarousel images={images} />
     </div>
   )
 }
