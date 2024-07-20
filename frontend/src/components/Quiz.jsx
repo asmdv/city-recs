9ba4001baecf732e5d55b6937a02c70262e7e748
Julia Gao <t-juliagao@microsoft.com>
2024-07-20 16:51:01 -0700

    update quiz

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -11,7 +11,7 @@ export default function Quiz() {
   const [isOther, setIsOther] = useState(false);
   const [formData, setFormData] = useState({
     occupation: '',
-    location: '',
+    music: '',
     password: '',
   });
 
@@ -45,50 +45,83 @@ export default function Quiz() {
           <FormControl id="occupation">
             <FormLabel>Occupation</FormLabel>
             <Select value={formData.occupation} onChange={handleChange} placeholder="Select your occupation">
-                <option value="Developer">Developer</option>
-                <option value="Designer">Designer</option>
-                <option value="Manager">Manager</option>
-                <option value="Doctor">Doctor</option>
-              </Select>
+              <option value="Accountant">Accountant</option>
+              <option value="Actor">Actor</option>
+              <option value="Artist">Artist</option>
+              <option value="Athlete">Athlete</option>
+              <option value="Chef">Chef</option>
+              <option value="Designer">Designer</option>
+              <option value="Developer">Developer</option>
+              <option value="Doctor">Doctor</option>
+              <option value="Engineer">Engineer</option>
+              <option value="Entrepreneur">Entrepreneur</option>
+              <option value="Firefighter">Firefighter</option>
+              <option value="Lawyer">Lawyer</option>
+              <option value="Manager">Manager</option>
+              <option value="Mechanic">Mechanic</option>
+              <option value="Musician">Musician</option>
+              <option value="Nurse">Nurse</option>
+              <option value="Pharmacist">Pharmacist</option>
+              <option value="Pilot">Pilot</option>
+              <option value="Police Officer">Police Officer</option>
+              <option value="Politician">Politician</option>
+              <option value="Salesperson">Salesperson</option>
+              <option value="Scientist">Scientist</option>
+              <option value="Social Worker">Social Worker</option>
+              <option value="Teacher">Teacher</option>
+              <option value="Veterinarian">Veterinarian</option>
+              <option value="Writer">Writer</option>
+            </Select>
           </FormControl>
         );
       case 1:
         return (
-          <FormControl id="location" >
-            <FormLabel>Location</FormLabel>
-            <Select value={formData.location} onChange={handleChange} placeholder="Select your location">
-              <option value="New York">New York</option>
-              <option value="Los Angeles">Los Angeles</option>
-              <option value="Chicago">Chicago</option>
-              <option value="Houston">Houston</option>
-              <option value="Phoenix">Phoenix</option>
+          <FormControl id="music" >
+            <FormLabel>Music Taste</FormLabel>
+            <Select value={formData.music} onChange={handleChange} placeholder="Select your music taste">
+            <option value="Blues">Blues</option>
+            <option value="Classical">Classical</option>
+            <option value="Country">Country</option>
+            <option value="Electronic">Electronic</option>
+            <option value="Folk">Folk</option>
+            <option value="Hip-Hop">Hip-Hop</option>
+            <option value="Indie">Indie</option>
+            <option value="Jazz">Jazz</option>
+            <option value="K-Pop">K-Pop</option>
+            <option value="Metal">Metal</option>
+            <option value="Pop">Pop</option>
+            <option value="Punk">Punk</option>
+            <option value="R&B">R&B</option>
+            <option value="Reggae">Reggae</option>
+            <option value="Rock">Rock</option>
+            <option value="Soul">Soul</option>
             </Select>
           </FormControl>
         );
-      case 2:
-        return (
-          <FormControl id="mbti">
-            <FormLabel>MBTI</FormLabel>
-            <Select value={formData.mbti} onChange={handleChange} placeholder="Select your MBTI">
-              <option value="INTJ">INTJ</option>
-              <option value="INTP">INTP</option>
-              <option value="ENTJ">ENTJ</option>
-              <option value="ENTP">ENTP</option>
-              <option value="INFJ">INFJ</option>
-              <option value="INFP">INFP</option>
-              <option value="ENFJ">ENFJ</option>
-              <option value="ENFP">ENFP</option>
-              <option value="ISTJ">ISTJ</option>
-              <option value="ISFJ">ISFJ</option>
-              <option value="ESTJ">ESTJ</option>
-              <option value="ESFJ">ESFJ</option>
-              <option value="ISTP">ISTP</option>
-              <option value="ISFP">ISFP</option>
-              <option value="ESTP">ESTP</option>
-              <option value="ESFP">ESFP</option>
-            </Select>
-          </FormControl>
-        );
+      // case 2:
+      //   return (
+      //     <FormControl id="mbti">
+      //       <FormLabel>MBTI</FormLabel>
+      //       <Select value={formData.mbti} onChange={handleChange} placeholder="Select your MBTI">
+      //         <option value="INTJ">INTJ</option>
+      //         <option value="INTP">INTP</option>
+      //         <option value="ENTJ">ENTJ</option>
+      //         <option value="ENTP">ENTP</option>
+      //         <option value="INFJ">INFJ</option>
+      //         <option value="INFP">INFP</option>
+      //         <option value="ENFJ">ENFJ</option>
+      //         <option value="ENFP">ENFP</option>
+      //         <option value="ISTJ">ISTJ</option>
+      //         <option value="ISFJ">ISFJ</option>
+      //         <option value="ESTJ">ESTJ</option>
+      //         <option value="ESFJ">ESFJ</option>
+      //         <option value="ISTP">ISTP</option>
+      //         <option value="ISFP">ISFP</option>
+      //         <option value="ESTP">ESTP</option>
+      //         <option value="ESFP">ESFP</option>
+      //       </Select>
+      //     </FormControl>
+      //   );
         case 3:
         return (
           <FormControl id="color">
@@ -106,11 +139,6 @@ export default function Quiz() {
         );
     }
   };
-
-
-
-
-
 
   return (
     <div>
