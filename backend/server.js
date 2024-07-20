4bc0ff5cbc6eed2da0fe5864f58a705bf882a547
Michelle Chang <changmi@seas.upenn.edu>
2024-07-20 14:27:33 -0700

    no node modules + backend update

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,11 @@ const db = new sqlite3.Database('cities.db', (err) => {
       console.error('Could not open database', err.message);
     } else {
       db.run(`
-        CREATE TABLE IF NOT EXISTS responses (
+        CREATE TABLE IF NOT EXISTS cities (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           name TEXT,
-          occupation TEXT,
-          customOccupation TEXT,
-          password TEXT,
-          mbti TEXT
+          imgpath TEXT,
+          info TEXT
         )
       `);
     }
@@ -33,16 +31,19 @@ app.use(cors({
   }));
 
 app.get('/', async(req,res) => {
-    res.status(200).json({message: "Hello world"})
+    res.status(200).json({ message: "Hello world" })
 })
 
 
 // Endpoint to handle form submission
 app.post('/submit', (req, res) => {
     const { name, occupation, customOccupation, password, mbti } = req.body;
+
+    //Logic for req.body to cities goes here
+
     const query = `
-      INSERT INTO responses (name, occupation, customOccupation, password, mbti)
-      VALUES (?, ?, ?, ?, ?)
+      INSERT INTO cities (name, imgpath, info)
+      VALUES (?, ?, ?)
     `;
     db.run(query, [name, occupation, customOccupation, password, mbti], function(err) {
       if (err) {
@@ -61,7 +62,7 @@ app.get('/events', (req, res) => {
       return res.status(400).json({ error: 'City parameter is required' });
     }
   
-    const query = 'SELECT * FROM events WHERE city = ?';
+    const query = 'SELECT * FROM cities WHERE city = ?';
     db.all(query, [city], (err, rows) => {
       if (err) {
         return res.status(500).json({ error: err.message });
