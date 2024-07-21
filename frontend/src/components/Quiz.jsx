18b16325b1556437f0cdef88d9e204c155fcef67
Julia Gao <t-juliagao@microsoft.com>
2024-07-20 21:59:33 -0700

    update backend

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {  Box, Button, FormControl, FormLabel, Input, Stack, Text, Select, Progress } from '@chakra-ui/react';
 import { useState } from 'react';
 import Output from './Output';
-
+import axios from 'axios';
 
 export default function Quiz() {
 
@@ -41,6 +41,7 @@ export default function Quiz() {
     e.preventDefault();
     console.log(formData);
     setSubmitted(true);
+    handleSubmitButton();
     // You can add form submission logic here
   };
 
@@ -52,7 +53,7 @@ export default function Quiz() {
         return (
           <FormControl id="occupation">
             <FormLabel>Occupation</FormLabel>
-            <Select value={formData.occupation} onChange={handleChange} placeholder="Select your occupation">
+            <Select value={formData.occupation} onChange={handleChange} name="occupation" placeholder="Select your occupation">
               <option value="Accountant">Accountant</option>
               <option value="Actor">Actor</option>
               <option value="Artist">Artist</option>
@@ -86,24 +87,24 @@ export default function Quiz() {
         return (
           <FormControl id="music" >
             <FormLabel>Music Taste</FormLabel>
-            <Select value={formData.music} onChange={handleChange} placeholder="Select your music taste">
-            <option value="Blues">Blues</option>
-            <option value="Classical">Classical</option>
-            <option value="Country">Country</option>
-            <option value="Electronic">Electronic</option>
-            <option value="Folk">Folk</option>
-            <option value="Hip-Hop">Hip-Hop</option>
-            <option value="Indie">Indie</option>
-            <option value="Jazz">Jazz</option>
-            <option value="K-Pop">K-Pop</option>
-            <option value="Metal">Metal</option>
-            <option value="Pop">Pop</option>
-            <option value="Punk">Punk</option>
-            <option value="R&B">R&B</option>
-            <option value="Reggae">Reggae</option>
-            <option value="Rock">Rock</option>
-            <option value="Soul">Soul</option>
-            </Select>
+              <Select value={formData.music} onChange={handleChange} name="music" placeholder="Select your music taste">
+              <option value="Blues">Blues</option>
+              <option value="Classical">Classical</option>
+              <option value="Country">Country</option>
+              <option value="Electronic">Electronic</option>
+              <option value="Folk">Folk</option>
+              <option value="Hip-Hop">Hip-Hop</option>
+              <option value="Indie">Indie</option>
+              <option value="Jazz">Jazz</option>
+              <option value="K-Pop">K-Pop</option>
+              <option value="Metal">Metal</option>
+              <option value="Pop">Pop</option>
+              <option value="Punk">Punk</option>
+              <option value="R&B">R&B</option>
+              <option value="Reggae">Reggae</option>
+              <option value="Rock">Rock</option>
+              <option value="Soul">Soul</option>
+              </Select>
           </FormControl>
         );
       case 2:
@@ -158,9 +159,9 @@ export default function Quiz() {
         );
       case 7:
         return (
-          <FormControl id="Q&A">
+          <FormControl id="Q">
             <FormLabel>MBTI</FormLabel>
-            <Select value={formData.TvsF} onChange={handleChange} placeholder="In your free time, do you:">
+            <Select value={formData.Q} onChange={handleChange} placeholder="In your free time, do you:">
               <option value="Social">Seek out social activities</option>
               <option value="Isolated">Prefer to spend time alone or with a few close friends</option>
             </Select>
@@ -173,12 +174,27 @@ export default function Quiz() {
     }
   };
 
+  axios.defaults.baseURL = 'http://localhost:3000';
+  const handleSubmitButton = async () => {
+    var occupation = formData.occupation;
+    var music = formData.music;
+
+    try {
+      const response = await axios.post('/ask', { occupation, music });
+
+      console.log(response);
+    } catch (error) {
+      console.error('There was an error!', error.response);
+    }
+
+  }
+
   return (
     <div>
       {!submitted ? (
    <Box width="100%" maxWidth="500px" mx="auto" mt={10} p={5} borderWidth="1px" borderRadius="lg">
    <Progress value={(step / 8) * 100} mb={4} mt={3}/>
-        <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
           <Stack spacing={4}>
             {renderStep()}
 
